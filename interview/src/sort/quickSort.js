4f499f55257ee211a5eb4de9977ec64c4d4975e7
whjin <1414848321@qq.com>
2022-10-20 19:01:07 +0800

    update

--- interview/src/sort/quickSort.js
+++ interview/src/sort/quickSort.js
@@ -4,8 +4,7 @@ function quickSort(arr) {
   }
   let left = [],
     rigth = [],
-    index = Math.floor(arr.length / 2),
-    middle = arr.splice(index, 1)[0];
+    middle = arr.splice(Math.floor(arr.length / 2), 1);
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < middle) {
       left.push(arr[i]);
@@ -13,5 +12,5 @@ function quickSort(arr) {
       rigth.push(arr[i]);
     }
   }
-  return quickSort(left).concat([middle], quickSort(rigth));
+  return quickSort(left).concat(middle, quickSort(rigth));
 }
