2fc94cd081f0c4ffcb7ae046320f67fa06675c50
whjin <1414848321@qq.com>
2023-04-24 17:08:15 +0800

    update:禁用F12、鼠标右键、鼠标拖放，封装图片预览方法

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,18 +1,49 @@
 window.onload = function () {
-    const images = document.querySelectorAll(".control-section img");
-    images.forEach((image) => {
-        image.onclick = function (e) {
-            let target = e.target;
-            if (target.classList.contains("large")) {
-                target.classList.remove("large");
-            } else {
-                target.classList.add("large");
-            }
-            images.forEach(item => {
-                if (item !== target) {
-                    item.classList.remove("large");
+    document.oncontextmenu = function (e) {
+        return false;
+    };
+    document.ondragstart = function (e) {
+        return false;
+    };
+
+    document.onkeydown = function (e) {
+        if (e.code == "F12") {
+            return false;
+        }
+    };
+
+    // 管控主机
+    previewImg(".control-section", ".control-section img");
+    // 仓内屏
+    previewImg(".terminal-section", ".terminal-section img");
+
+    function previewImg (sectionEle, imgEle) {
+        const section = document.querySelector(sectionEle);
+        const images = document.querySelectorAll(imgEle);
+        images.forEach((image) => {
+            image.onclick = function (e) {
+                let target = e.target;
+                const imgNodes = document.querySelectorAll(imgEle);
+                let lastChild = imgNodes[imgNodes.length - 1];
+                const { src, alt, title } = target;
+                let img = document.createElement("img");
+                img.src = src;
+                img.alt = alt;
+                img.title = title;
+                img.classList.add("preview");
+                if (lastChild.classList.contains("preview")) {
+                    if (lastChild.alt == target.alt) {
+                        section.removeChild(lastChild);
+                    } else {
+                        section.replaceChild(img, lastChild);
+                    }
+                } else {
+                    section.appendChild(img);
                 }
-            });
-        };
-    });
+                img.onclick = function () {
+                    section.removeChild(img);
+                };
+            };
+        });
+    }
 };
