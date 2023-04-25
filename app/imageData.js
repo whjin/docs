abebb6e25a495193e4f039117bbdd0320f1abec2
whjin <1414848321@qq.com>
2023-04-25 22:07:36 +0800

    update

--- app/imageData.js
+++ app/imageData.js
@@ -1,66 +1,66 @@
 const controlList = [
   {
-    "src": "/app/control/home.png",
+    "src": "app/images/control/home.png",
     "alt": "home",
     "title": "首页"
   },
   {
-    "src": "/app/control/intercom_1.png",
+    "src": "app/images/control/intercom_1.png",
     "alt": "intercom_1",
     "title": "对讲"
   },
   {
-    "src": "/app/control/intercom_2.png",
+    "src": "app/images/control/intercom_2.png",
     "alt": "intercom_2",
     "title": "对讲"
   },
   {
-    "src": "/app/control/intercom_3.png",
+    "src": "app/images/control/intercom_3.png",
     "alt": "intercom_3",
     "title": "对讲"
   },
   {
-    "src": "../app/control/intercom_4.png",
+    "src": "app/images/control/intercom_4.png",
     "alt": "intercom_4",
     "title": "对讲"
   },
   {
-    "src": "../app/control/radio.png",
+    "src": "app/images/control/radio.png",
     "alt": "radio",
     "title": "广播"
   },
   {
-    "src": "../app/control/audio_1.png",
+    "src": "app/images/control/audio_1.png",
     "alt": "audio_1",
     "title": "音频"
   },
   {
-    "src": "../app/control/audio_2.png",
+    "src": "app/images/control/audio_2.png",
     "alt": "audio_2",
     "title": "音频"
   },
   {
-    "src": "../app/control/video_1.png",
+    "src": "app/images/control/video_1.png",
     "alt": "video_1",
     "title": "视频"
   },
   {
-    "src": "../app/control/video_2.png",
+    "src": "app/images/control/video_2.png",
     "alt": "video_2",
     "title": "视频"
   },
   {
-    "src": "../app/control/setting_1.png",
+    "src": "app/images/control/setting_1.png",
     "alt": "setting_1",
     "title": "设置"
   },
   {
-    "src": "../app/control/setting_2.png",
+    "src": "app/images/control/setting_2.png",
     "alt": "setting_2",
     "title": "设置"
   },
   {
-    "src": "../app/control/setting_2.png",
+    "src": "app/images/control/setting_2.png",
     "alt": "setting_2",
     "title": "设置"
   }
