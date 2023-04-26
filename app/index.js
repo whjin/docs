dc7e4bd94619610ef2dc2c777ac99d139530ae0f
whjin <1414848321@qq.com>
2023-04-26 23:53:21 +0800

    add:仓内屏、仓外屏、调试工具、管理后台

--- app/index.js
+++ app/index.js
@@ -7,61 +7,59 @@ document.ondragstart = function (e) {
 
 let isF12 = false;
 document.onkeydown = function (e) {
-    if (e.code == "F12") {
-        return isF12;
+    if (e.key == "F12") {
+        // return isF12;
     }
 
 };
 document.onkeyup = function (e) {
-    if (e.key == "MediaTrackPrevious") {
-        isF12 = false;
-    }
-    if (e.key == "MediaTrackNext") {
+    if (e.key == "O") {
         isF12 = true;
+    }
+    if (e.key == "L") {
+        isF12 = false;
     }
 };
 
 // 管控主机
-previewImage(".control-section", ".control-section canvas");
+previewImage(controlList, ".control-section", ".control-section canvas");
 // 仓内屏
-// previewImage(".terminal-section", ".terminal-section img");
+previewImage(terminalList, ".terminal-section", ".terminal-section canvas");
 
-function previewImage (sectionEle, canvasEle) {
+function previewImage (list, sectionEle, canvasEle) {
     const section = document.querySelector(sectionEle);
 
-    controlList.forEach(imageData => {
-        const canvas = drawImageToCanvas(imageData, 96, 54);
+    list.forEach(imageData => {
+        const canvas = drawImageToCanvas(imageData, 192, 108);
         section.appendChild(canvas);
     });
 
-    window.onload = function () {
-        const childNodes = [...section.childNodes];
-        childNodes.forEach((node) => {
-            node.onclick = function (e) {
-                let target = e.target;
-                let src = target.getAttribute("src");
-                let alt = target.getAttribute("alt");
-                let title = target.getAttribute("title");
-                const imageData = { src, alt, title };
-                const canvas = drawImageToCanvas(imageData, 768, 432);
-                const canvasNodes = document.querySelectorAll(canvasEle);
-                let lastChild = canvasNodes[canvasNodes.length - 1];
-                canvas.classList.add("preview");
-                if (lastChild.classList.contains("preview")) {
-                    if (lastChild.getAttribute("alt") == alt) {
-                        section.removeChild(lastChild);
-                    } else {
-                        section.replaceChild(canvas, lastChild);
-                    }
+    const childNodes = [...section.childNodes];
+    childNodes.forEach((node) => {
+        node.onclick = function (e) {
+            let target = e.target;
+            let src = target.getAttribute("src");
+            let alt = target.getAttribute("alt");
+            let title = target.getAttribute("title");
+            const imageData = { src, alt, title };
+            const canvas = drawImageToCanvas(imageData, 768, 432);
+            const canvasNodes = document.querySelectorAll(canvasEle);
+            let lastChild = canvasNodes[canvasNodes.length - 1];
+            canvas.classList.add("preview");
+            if (lastChild.classList.contains("preview")) {
+                if (lastChild.getAttribute("alt") == alt) {
+                    section.removeChild(lastChild);
                 } else {
-                    section.appendChild(canvas);
+                    section.replaceChild(canvas, lastChild);
                 }
-                canvas.onclick = function () {
-                    section.removeChild(canvas);
-                };
+            } else {
+                section.appendChild(canvas);
+            }
+            canvas.onclick = function () {
+                section.removeChild(canvas);
             };
-        });
-    };
+        };
+    });
 }
 
 function drawImageToCanvas (imageData, width, height) {
