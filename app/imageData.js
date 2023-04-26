72e798c9e66112cb8f04a28941f16d9fd9df5497
whjin <1414848321@qq.com>
2023-04-26 17:34:22 +0800

    update

--- app/imageData.js
+++ app/imageData.js
@@ -1,66 +1,66 @@
 const controlList = [
   {
-    "src": "app/images/control/home.png",
+    "src": "app/images/control/home.jpg",
     "alt": "home",
     "title": "首页"
   },
   {
-    "src": "app/images/control/intercom_1.png",
+    "src": "app/images/control/intercom_1.jpg",
     "alt": "intercom_1",
     "title": "对讲"
   },
   {
-    "src": "app/images/control/intercom_2.png",
+    "src": "app/images/control/intercom_2.jpg",
     "alt": "intercom_2",
     "title": "对讲"
   },
   {
-    "src": "app/images/control/intercom_3.png",
+    "src": "app/images/control/intercom_3.jpg",
     "alt": "intercom_3",
     "title": "对讲"
   },
   {
-    "src": "app/images/control/intercom_4.png",
+    "src": "app/images/control/intercom_4.jpg",
     "alt": "intercom_4",
     "title": "对讲"
   },
   {
-    "src": "app/images/control/radio.png",
+    "src": "app/images/control/radio.jpg",
     "alt": "radio",
     "title": "广播"
   },
   {
-    "src": "app/images/control/audio_1.png",
+    "src": "app/images/control/audio_1.jpg",
     "alt": "audio_1",
     "title": "音频"
   },
   {
-    "src": "app/images/control/audio_2.png",
+    "src": "app/images/control/audio_2.jpg",
     "alt": "audio_2",
     "title": "音频"
   },
   {
-    "src": "app/images/control/video_1.png",
+    "src": "app/images/control/video_1.jpg",
     "alt": "video_1",
     "title": "视频"
   },
   {
-    "src": "app/images/control/video_2.png",
+    "src": "app/images/control/video_2.jpg",
     "alt": "video_2",
     "title": "视频"
   },
   {
-    "src": "app/images/control/setting_1.png",
+    "src": "app/images/control/setting_1.jpg",
     "alt": "setting_1",
     "title": "设置"
   },
   {
-    "src": "app/images/control/setting_2.png",
+    "src": "app/images/control/setting_2.jpg",
     "alt": "setting_2",
     "title": "设置"
   },
   {
-    "src": "app/images/control/setting_3.png",
+    "src": "app/images/control/setting_3.jpg",
     "alt": "setting_3",
     "title": "设置"
   }
