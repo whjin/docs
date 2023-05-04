1c8d30d8a54d7a722ac6127120badd8568f956a4
whjin <1414848321@qq.com>
2023-05-04 09:03:26 +0800

    update

--- app/static/controlList.js
+++ app/static/controlList.js
@@ -10,44 +10,49 @@ const controlList = [
     "title": "管控主机_首页"
   },
   {
-    "src": "",
+    "src": "app/images/control/intercom.png",
+    "alt": "control_intercom",
+    "title": "管控主机_对讲"
+  },
+  {
+    "src": "app/images/control/intercom_1.png",
     "alt": "control_intercom_1",
     "title": "管控主机_对讲"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/radio.png",
+    "alt": "control_radio",
+    "title": "管控主机_广播"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/audio.png",
+    "alt": "control_audio",
+    "title": "管控主机_音频"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/audio_group.png",
+    "alt": "control_audio_group",
+    "title": "管控主机_音频"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/video.png",
+    "alt": "control_video",
+    "title": "管控主机_视频"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/setting_1.png",
+    "alt": "control_setting_1",
+    "title": "管控主机_设置"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/setting_2.png",
+    "alt": "control_setting_2",
+    "title": "管控主机_设置"
   },
   {
-    "src": "",
-    "alt": "control_intercom_1",
-    "title": "管控主机_对讲"
+    "src": "app/images/control/setting_3.png",
+    "alt": "control_setting_3",
+    "title": "管控主机_设置"
   },
 ];
 
