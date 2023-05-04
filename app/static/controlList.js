bcf70d57b0225932327d0f63917c7224d3facbfc
whjin <1414848321@qq.com>
2023-05-04 10:36:52 +0800

    update

--- app/static/controlList.js
+++ app/static/controlList.js
@@ -1,108 +1,68 @@
 const controlList = [
   {
-    "src": "app/images/control/home.png",
+    "src": "app/images/control/home.jpg",
     "alt": "control_home",
     "title": "管控主机_首页"
   },
   {
-    "src": "app/images/control/home_1.png",
-    "alt": "control_home_1",
-    "title": "管控主机_首页"
-  },
-  {
-    "src": "app/images/control/intercom.png",
-    "alt": "control_intercom",
-    "title": "管控主机_对讲"
-  },
-  {
-    "src": "app/images/control/intercom_1.png",
+    "src": "app/images/control/intercom_1.jpg",
     "alt": "control_intercom_1",
     "title": "管控主机_对讲"
   },
   {
-    "src": "app/images/control/intercom_2.png",
+    "src": "app/images/control/intercom_2.jpg",
     "alt": "control_intercom_2",
     "title": "管控主机_对讲"
   },
   {
-    "src": "app/images/control/intercom_3.png",
+    "src": "app/images/control/intercom_3.jpg",
     "alt": "control_intercom_3",
     "title": "管控主机_对讲"
   },
   {
-    "src": "app/images/control/intercom_4.png",
+    "src": "app/images/control/intercom_4.jpg",
     "alt": "control_intercom_4",
     "title": "管控主机_对讲"
   },
   {
-    "src": "app/images/control/radio.png",
+    "src": "app/images/control/radio.jpg",
     "alt": "control_radio",
     "title": "管控主机_广播"
   },
   {
-    "src": "app/images/control/radio_1.png",
-    "alt": "control_radio_1",
-    "title": "管控主机_广播"
-  },
-  {
-    "src": "app/images/control/audio.png",
-    "alt": "control_audio",
-    "title": "管控主机_音频"
-  },
-  {
-    "src": "app/images/control/audio_1.png",
+    "src": "app/images/control/audio_1.jpg",
     "alt": "control_audio_1",
     "title": "管控主机_音频"
   },
   {
-    "src": "app/images/control/audio_2.png",
+    "src": "app/images/control/audio_2.jpg",
     "alt": "control_audio_2",
     "title": "管控主机_音频"
   },
   {
-    "src": "app/images/control/audio_3.png",
-    "alt": "control_audio_3",
-    "title": "管控主机_音频"
-  },
-  {
-    "src": "app/images/control/audio_4.png",
-    "alt": "control_audio_4",
-    "title": "管控主机_音频"
-  },
-  {
-    "src": "app/images/control/video.png",
-    "alt": "control_video",
-    "title": "管控主机_视频"
-  },
-  {
-    "src": "app/images/control/video_1.png",
+    "src": "app/images/control/video_1.jpg",
     "alt": "control_video_1",
     "title": "管控主机_视频"
   },
   {
-    "src": "app/images/control/video_2.png",
+    "src": "app/images/control/video_2.jpg",
     "alt": "control_video_2",
     "title": "管控主机_视频"
   },
   {
-    "src": "app/images/control/video_3.png",
-    "alt": "control_video_3",
-    "title": "管控主机_视频"
-  },
-  {
-    "src": "app/images/control/setting_1.png",
+    "src": "app/images/control/setting_1.jpg",
     "alt": "control_setting_1",
     "title": "管控主机_设置"
   },
   {
-    "src": "app/images/control/setting_2.png",
+    "src": "app/images/control/setting_2.jpg",
     "alt": "control_setting_2",
     "title": "管控主机_设置"
   },
   {
-    "src": "app/images/control/setting_3.png",
+    "src": "app/images/control/setting_3.jpg",
     "alt": "control_setting_3",
     "title": "管控主机_设置"
-  },
+  }
 ];
 
