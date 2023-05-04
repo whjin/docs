a4cdf542ead2d62a0363e8c6c68e3b55fe0acce0
whjin <1414848321@qq.com>
2023-05-04 10:11:36 +0800

    update

--- app/static/controlList.js
+++ app/static/controlList.js
@@ -20,8 +20,28 @@ const controlList = [
     "title": "管控主机_对讲"
   },
   {
+    "src": "app/images/control/intercom_2.png",
+    "alt": "control_intercom_2",
+    "title": "管控主机_对讲"
+  },
+  {
+    "src": "app/images/control/intercom_3.png",
+    "alt": "control_intercom_3",
+    "title": "管控主机_对讲"
+  },
+  {
+    "src": "app/images/control/intercom_4.png",
+    "alt": "control_intercom_4",
+    "title": "管控主机_对讲"
+  },
+  {
     "src": "app/images/control/radio.png",
     "alt": "control_radio",
+    "title": "管控主机_广播"
+  },
+  {
+    "src": "app/images/control/radio_1.png",
+    "alt": "control_radio_1",
     "title": "管控主机_广播"
   },
   {
@@ -30,13 +50,43 @@ const controlList = [
     "title": "管控主机_音频"
   },
   {
-    "src": "app/images/control/audio_group.png",
-    "alt": "control_audio_group",
+    "src": "app/images/control/audio_1.png",
+    "alt": "control_audio_1",
+    "title": "管控主机_音频"
+  },
+  {
+    "src": "app/images/control/audio_2.png",
+    "alt": "control_audio_2",
+    "title": "管控主机_音频"
+  },
+  {
+    "src": "app/images/control/audio_3.png",
+    "alt": "control_audio_3",
+    "title": "管控主机_音频"
+  },
+  {
+    "src": "app/images/control/audio_4.png",
+    "alt": "control_audio_4",
     "title": "管控主机_音频"
   },
   {
     "src": "app/images/control/video.png",
     "alt": "control_video",
+    "title": "管控主机_视频"
+  },
+  {
+    "src": "app/images/control/video_1.png",
+    "alt": "control_video_1",
+    "title": "管控主机_视频"
+  },
+  {
+    "src": "app/images/control/video_2.png",
+    "alt": "control_video_2",
+    "title": "管控主机_视频"
+  },
+  {
+    "src": "app/images/control/video_3.png",
+    "alt": "control_video_3",
     "title": "管控主机_视频"
   },
   {
