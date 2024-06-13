2afc4ec5f4f5dc547dfb8c06accbe418e073903b
whjin <wuhuajin09@163.com>
2024-06-13 22:46:19 +0800

    feat: 修改图片图床链接

--- project/app/static/behaviorList.js
+++ project/app/static/behaviorList.js
@@ -1,52 +1,112 @@
 const behaviorList = [
   {
-    src: "project/app/images/behavior/behavior_1.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b668e00.jpg",
     alt: "behavior_1",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_2.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b687798.jpg",
     alt: "behavior_2",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_3.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b69a0ab.jpg",
     alt: "behavior_3",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_4.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b6a5dbf.jpg",
     alt: "behavior_4",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_5.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b6b3a61.jpg",
     alt: "behavior_5",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_6.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b80812c.jpg",
     alt: "behavior_6",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_7.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b89168e.jpg",
     alt: "behavior_7",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_8.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b911ca9.jpg",
     alt: "behavior_8",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_9.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04b9b2a49.jpg",
     alt: "behavior_9",
     title: "视频智能行为分析系统",
   },
   {
-    src: "project/app/images/behavior/behavior_10.jpg",
+    src: "https://img.picui.cn/free/2024/06/13/666b04ba79127.jpg",
     alt: "behavior_10",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bad97ed.jpg",
+    alt: "behavior_11",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bb83521.jpg",
+    alt: "behavior_12",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bbd6366.jpg",
+    alt: "behavior_13",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bcaffa0.jpg",
+    alt: "behavior_14",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bd1d757.jpg",
+    alt: "behavior_15",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bdd4e55.jpg",
+    alt: "behavior_16",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04be32276.jpg",
+    alt: "behavior_17",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04be9ff39.jpg",
+    alt: "behavior_18",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bf6229e.jpg",
+    alt: "behavior_19",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04bfcf824.jpg",
+    alt: "behavior_20",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04c0667cc.jpg",
+    alt: "behavior_21",
+    title: "视频智能行为分析系统",
+  },
+  {
+    src: "https://img.picui.cn/free/2024/06/13/666b04c11d6a3.jpg",
+    alt: "behavior_22",
     title: "视频智能行为分析系统",
   }
 ];
