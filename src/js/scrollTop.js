0930a7124bc31df9fd0fdc239a7dce7d69ea56e1
whjin <wuhuajin09@163.com>
2026-04-10 03:40:49 +0800

    更新文档

--- src/js/scrollTop.js
+++ src/js/scrollTop.js
@@ -1,18 +1,26 @@
 $(function () {
   $(window).scroll(function () {
-    var $Height = $(window).scrollTop();
+    let $Height = $(window).scrollTop();
     if ($Height === 0) {
+      $('.back-btn').removeClass('hidden');
       $('.scroll-top').addClass('opacity');
     } else {
+      $('.back-btn').addClass('hidden');
       $('.scroll-top').removeClass('opacity');
     }
     if ($Height > 100) {
+      $('.back-btn').fadeOut(500);
       $('.scroll-top').fadeIn(500);
     } else {
+      $('.back-btn').fadeIn(500);
       $('.scroll-top').fadeOut(500);
     }
   });
   $('.scroll-top').on('click', function () {
     $('html,body').animate({ scrollTop: 0 }, 500);
+  });
+  $('.back-btn').on('click', function () {
+    const targetUrl = window.location.origin;
+    window.location.href = targetUrl;
   });
 });
