f7da599fe24e26c85c06e396291bb98c03599b6f
whjin <wuhuajin09@163.com>
2026-04-10 14:36:27 +0800

    feat: shell部署脚本

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-echo "正在提交代码..."
-git pull
-git add .
-
 COMMIT_FILE="commit.md"
 DEFAULT_MESSAGE="提交更新"
 TMP_COMMIT=$(mktemp -t commit.XXXXXX 2>/dev/null || mktemp)
@@ -17,14 +13,21 @@ fi
 # 过滤空行 → 取第一行 → 剔除开头时间戳 → 去除首尾空白（避免纯空格干扰）
 latest_commit=$(sed '/^[[:space:]]*$/d' "$COMMIT_FILE" | head -n 1 | sed -E 's/^[0-9-]+ [0-9:.]+ //i' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
+echo "正在提交代码..."
+# git pull
+# git add .
+
 # 3. 判断内容是否为空，若为空则使用默认提交信息
 if [ -n "$latest_commit" ]; then 
-    git commit -F "$TMP_COMMIT"
-else  
-    git commit -m "$DEFAULT_MESSAGE"
+    # echo "$latest_commit" > "$TMP_COMMIT"
+    echo "11111111：$latest_commit"
+    # git commit -F "$TMP_COMMIT"
+else  # 无有效信息，使用默认值
+    echo "2222222：$DEFAULT_MESSAGE"
+    # git commit -m "$DEFAULT_MESSAGE"
 fi
 
-git push 
+# git push 
 
 echo "按任意键关闭"
 read -n 1
