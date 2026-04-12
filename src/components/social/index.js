1acd429c4785f09b4a8c82eb8c111b1c174c7c92
whjin <wuhuajin09@163.com>
2026-04-12 16:43:11 +0800

    feat: 适配移动端

--- src/components/social/index.js
+++ src/components/social/index.js
@@ -1,45 +1,61 @@
 (function () {
   const socials = [
     {
-      href: 'https://weibo.com/u/1710899102',
-      title: '微博',
-      name: 'weibo',
-      icon: 24,
-    },
-    {
-      href: 'https://wuhuajin.com',
-      title: '博客',
-      name: 'blog',
+      href: "",
+      title: "展开",
+      name: "expand",
       icon: 26,
     },
     {
-      href: 'https://github.com/whjin',
-      title: 'Github',
-      name: 'github',
+      href: "https://weibo.com/u/1710899102",
+      title: "微博",
+      name: "weibo",
       icon: 24,
     },
     {
-      href: 'mailto:wuhuajin09@163.com',
-      title: '邮箱',
-      name: 'email',
+      href: "https://wuhuajin.com",
+      title: "博客",
+      name: "blog",
+      icon: 26,
+    },
+    {
+      href: "https://github.com/whjin",
+      title: "Github",
+      name: "github",
+      icon: 24,
+    },
+    {
+      href: "mailto:wuhuajin09@163.com",
+      title: "邮箱",
+      name: "email",
       icon: 28,
     },
   ];
 
-  const titleEl = document.querySelector('.title');
-  const navEl = document.createElement('nav');
-  navEl.className = 'nav';
+  function isMobile() {
+    const ua = navigator.userAgent;
+    const isMobileUA =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    return isMobileUA;
+  }
 
-  socials.forEach(s => {
-    const aEl = document.createElement('a');
-    const imgEl = document.createElement('img');
+  const titleEl = document.querySelector(".title");
+
+  const expandEl = document.createElement("img");
+
+  const navEl = document.createElement("nav");
+  navEl.className = "nav";
+
+  socials.forEach((s) => {
+    const aEl = document.createElement("a");
+    const imgEl = document.createElement("img");
 
     aEl.href = s.href;
-    aEl.rel = 'noopener noreferrer';
-    aEl.target = '_blank';
+    aEl.rel = "noopener noreferrer";
+    aEl.target = "_blank";
     aEl.title = s.title;
+    aEl.className = `icon-${s.name}`;
 
-    imgEl.className = `icon-${s.name}`;
     imgEl.src = `src/images/icons/${s.name}.png`;
     imgEl.alt = s.title;
     imgEl.width = imgEl.height = s.icon;
