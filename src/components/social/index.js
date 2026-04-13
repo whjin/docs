a4e1237747fc50895a27535277797d44f6032d0a
whjin <wuhuajin09@163.com>
2026-04-13 08:39:58 +0800

    feat: 调整头部和底部导航栏样式

--- src/components/social/index.js
+++ src/components/social/index.js
@@ -1,58 +1,50 @@
+function isMobile() {
+  const ua = navigator.userAgent;
+  const isMobileUA =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+  return isMobileUA;
+}
+
 (function () {
   const socials = [
     {
-      href: "",
-      title: "展开",
-      name: "expand",
-      icon: 26,
-    },
-    {
-      href: "https://weibo.com/u/1710899102",
-      title: "微博",
-      name: "weibo",
+      href: 'https://weibo.com/u/1710899102',
+      title: '微博',
+      name: 'weibo',
       icon: 24,
     },
     {
-      href: "https://wuhuajin.com",
-      title: "博客",
-      name: "blog",
+      href: 'https://wuhuajin.com',
+      title: '博客',
+      name: 'blog',
       icon: 26,
     },
     {
-      href: "https://github.com/whjin",
-      title: "Github",
-      name: "github",
+      href: 'https://github.com/whjin',
+      title: 'Github',
+      name: 'github',
       icon: 24,
     },
     {
-      href: "mailto:wuhuajin09@163.com",
-      title: "邮箱",
-      name: "email",
+      href: 'mailto:wuhuajin09@163.com',
+      title: '邮箱',
+      name: 'email',
       icon: 28,
     },
   ];
 
-  function isMobile() {
-    const ua = navigator.userAgent;
-    const isMobileUA =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
-    return isMobileUA;
-  }
+  const titleEl = document.querySelector('.title');
 
-  const titleEl = document.querySelector(".title");
-
-  const expandEl = document.createElement("img");
-
-  const navEl = document.createElement("nav");
-  navEl.className = "nav";
+  const navEl = document.createElement('nav');
+  navEl.className = 'nav';
 
   socials.forEach((s) => {
-    const aEl = document.createElement("a");
-    const imgEl = document.createElement("img");
+    const aEl = document.createElement('a');
+    const imgEl = document.createElement('img');
 
     aEl.href = s.href;
-    aEl.rel = "noopener noreferrer";
-    aEl.target = "_blank";
+    aEl.rel = 'noopener noreferrer';
+    aEl.target = '_blank';
     aEl.title = s.title;
     aEl.className = `icon-${s.name}`;
 
@@ -65,4 +57,7 @@
   });
 
   titleEl.after(navEl);
+
+  const headerEl = document.querySelector('.header');
+  headerEl.style.justifyContent = isMobile() ? 'flex-start' : 'center';
 })();
